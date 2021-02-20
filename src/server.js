d46a627e0815af2c7e9be47fedd5d82b944dd64a
kdu <kdu.eduardo97@gmail.com>
2021-02-20 16:46:28 -0300

    Connect to mongodb

--- src/server.js
+++ src/server.js
@@ -1,37 +1,55 @@
 "use strict";
 
 const express = require("express");
-const bodyParser = require('body-parser');
-const port = 3000
+const bodyParser = require("body-parser");
+const port = 3000;
+const MongoClient = require("mongodb").MongoClient;
+
+const uri =
+  "mongodb+srv://espacoAdmin:Entm1844@clusterespaco1.2bj1f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+let newsCollection = ''
+  
+MongoClient.connect(uri, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to Database");
+    const db = client.db("News");
+    newsCollection = db.collection("news");
+  })
+  .catch(console.error);
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("API funcionando corretamente"));
 
 let news = [
   {
-    "title": 'kduzera',
-    'content': 'reeeeeeeee'
+    title: "kduzera",
+    content: "reeeeeeeee",
   },
   {
-    "title": 'Carlos',
-    'content': 'opaaaa'
-  }
-]
+    title: "Carlos",
+    content: "opaaaa",
+  },
+];
 
 app.get("/news", (req, res) => {
-  res.json(news)
+  res.json(news);
 });
 
-app.post('/news', (req,res) => {
-  news.push(req.body)
-  res.json({"status": "200 - Item adicionado com sucesso!"})
-})
+app.post("/news", (req, res) => {
+  newsCollection
+    .insertOne(req.body)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => console.error(error));
+});
 
 console.log("\nAPI rodando em http://localhost:3000/");
 
 app.listen(port, () => {
-  console.log(`Server is running at localhost:${port}`)
-})
+  console.log(`Server is running at localhost:${port}`);
+});
