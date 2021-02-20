e36bb5d29b1fc6e6d1f34e273f7f0f3b514c0382
kdu <kdu.eduardo97@gmail.com>
2021-02-20 16:59:07 -0300

    add view db

--- src/server.js
+++ src/server.js
@@ -8,12 +8,13 @@ const MongoClient = require("mongodb").MongoClient;
 const uri =
   "mongodb+srv://espacoAdmin:Entm1844@clusterespaco1.2bj1f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-let newsCollection = ''
-  
+let newsCollection = "";
+let db = "";
+
 MongoClient.connect(uri, { useUnifiedTopology: true })
   .then((client) => {
     console.log("Connected to Database");
-    const db = client.db("News");
+    db = client.db("News");
     newsCollection = db.collection("news");
   })
   .catch(console.error);
@@ -24,19 +25,15 @@ app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("API funcionando corretamente"));
 
-let news = [
-  {
-    title: "kduzera",
-    content: "reeeeeeeee",
-  },
-  {
-    title: "Carlos",
-    content: "opaaaa",
-  },
-];
-
 app.get("/news", (req, res) => {
-  res.json(news);
+  const news = db
+    .collection("news")
+    .find()
+    .toArray()
+    .then((results) => {
+      console.log(results);
+      res.json(results)
+    });
 });
 
 app.post("/news", (req, res) => {
